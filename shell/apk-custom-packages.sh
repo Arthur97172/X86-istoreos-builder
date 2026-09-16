0b1b856558578defc39fc4ce48f1888476c2e9d6
Arthur97172 <arthur97172@outlook.com>
2026-09-16 12:42:23 +0800

    Add files via upload

---
+++ shell/apk-custom-packages.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+# 25.12.x 第三方插件配置 (APK 格式) - x86-64 专用
+# 启用第三方插件时取消对应注释
+
+#动态DNS
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES ddns-scripts-cloudflare luci-app-ddns luci-i18n-ddns-zh-cn"
+
+#samba4
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-samba4 luci-i18n-samba4-zh-cn"
+
+#timecontrol上网时间控制
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-timecontrol luci-i18n-timecontrol-zh-cn"
+
+#watchdog看门狗
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES watchdog luci-app-watchdog luci-i18n-watchdog-zh-cn"
+
+# adguardhome广告拦截 (adguardhome)
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES adguardhome luci-app-adguardhome luci-i18n-adguardhome-zh-cn"
+
+# argon主题 (argon)
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-theme-argon luci-app-argon-config luci-i18n-argon-config-zh-cn"
+
+# material (material)
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-theme-material"
+
+# aurora主题 (aurora)
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-aurora-config luci-theme-aurora luci-i18n-aurora-config-zh-cn"
+
+# bandix-plus网络流量监控 (bandix-plus)
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES bandix-plus luci-app-bandix-plus luci-i18n-bandix-plus-zh-cn"
+
+# bandix网络流量监控 (bandix)
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES bandix luci-app-bandix luci-i18n-bandix-zh-cn"
+
+# clashoo代理面板 (clashoo)
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES clashoo luci-app-clashoo luci-i18n-clashoo-zh-cn"
+
+# cpu状态
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-cpu-status"
+
+# daede代理面板 (daede)
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES dae daed luci-app-daede"
+
+# Easymesh无线组网(easymesh)
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-easymesh luci-i18n-easymesh-zh-cn"
+
+# lucky内网穿透 (lucky)
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES lucky luci-app-lucky luci-i18n-lucky-zh-cn"
+
+# MosDNS转发器 (MosDNS)
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES mosdns luci-app-mosdns luci-i18n-mosdns-zh-cn"
+
+# netwizard网络设置向导 (netwizard)
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-netwizard luci-i18n-netwizard-zh-cn"
+
+# nikki代理面板 (nikki)
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES nikki luci-app-nikki luci-i18n-nikki-zh-cn"
+
+# openclash代理面板 (openclash)
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-openclash"
+
+# partexp分区扩容 (partexp)
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-partexp luci-i18n-partexp-zh-cn"
+
+# passwall依赖 (passwall)
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES geoview"
+
+# passwall代理面板 (passwall)
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-passwall luci-i18n-passwall-zh-cn"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES sing-box chinadns-ng geoview shadowsocksr-libev-ssr-local shadowsocksr-libev-ssr-redir"
+
+# poweroffdevice关机 (poweroffdevice)
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-poweroffdevice luci-i18n-poweroffdevice-zh-cn"
+
+# rtp2httpd流媒体转发 (rtp2httpd)
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES rtp2httpd luci-app-rtp2httpd luci-i18n-rtp2httpd-zh-cn"
+
+# run插件安装工具 (run)
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-run"
+
+# Netspeedtest网速测试 (netspeedtest)
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-netspeedtest luci-i18n-netspeedtest-zh-cn"
+
+# tailscaleVPN代理 (tailscale)
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES tailscale luci-app-tailscale luci-i18n-tailscale-zh-cn"
+
+# taskplan定时任务 (taskplan)
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-taskplan luci-i18n-taskplan-zh-cn"
